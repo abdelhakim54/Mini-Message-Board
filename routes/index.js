ef6a3d4cbc51577b83e35980835650d06a872c30
Abdelhakim <elouakouakabdelhakim@gmail.com>
2023-03-01 13:15:41 +0100

    handle the post request of the form view

--- routes/index.js
+++ routes/index.js
@@ -28,5 +28,9 @@ router.get('/', function(req, res, next) {
 router.get("/new", function(req, res, next){
   res.render("form");
 });
+router.post("/new", function(req, res, next){
+  messages.push({text:req.body.text, user:req.body.user, added: new Date()})
+  res.redirect("/")
+})
 
 module.exports = router;
